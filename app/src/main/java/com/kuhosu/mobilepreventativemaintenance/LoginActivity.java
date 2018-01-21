7104a8790736244f8385df9d93633fca06dc288b
muazz1m <>
2018-01-21 14:16:07 +0800

    finish execute pm ui

--- app/src/main/java/com/kuhosu/mobilepreventativemaintenance/LoginActivity.java
+++ app/src/main/java/com/kuhosu/mobilepreventativemaintenance/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_remarks);
+        setContentView(R.layout.activity_execute_pm);
 
 //        Button loginBtn = (Button) findViewById(R.id.loginBtn);
 //
