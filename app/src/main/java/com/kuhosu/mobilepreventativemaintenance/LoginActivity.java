11578aa3cb20d63f06ab53a5bc95bbd936ae5905
muazz1m <>
2018-01-21 15:52:58 +0800

    add qualitative ui

--- app/src/main/java/com/kuhosu/mobilepreventativemaintenance/LoginActivity.java
+++ app/src/main/java/com/kuhosu/mobilepreventativemaintenance/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_execute_pm);
+        setContentView(R.layout.activity_qualitative_task);
 
 //        Button loginBtn = (Button) findViewById(R.id.loginBtn);
 //
